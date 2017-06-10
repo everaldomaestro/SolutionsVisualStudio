eb858f4ec9f16e989db2f03189801680befbf735
Everaldo Almeida <everaldo.maestro@gmail.com>
2017-06-10 12:39:51 -0300

    Add Classe Transaction

--- TesteTransacao/TesteTransacao/Program.cs
+++ TesteTransacao/TesteTransacao/Program.cs
@@ -6,7 +6,17 @@ namespace TesteTransacao
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            {
+                Transaction tr = new Transaction();
+                string[] cmds = new string[2];
+
+                cmds[0] = "insert into Teste (Nome, Idade) values ('TESTE',19)";
+                cmds[1] = "insert into Teste (Nome, Idade) values (19,19)";
+
+                tr.transacao(cmds);
+
+                Console.ReadLine();
+            }
         }
     }
 }
---
+++ TesteTransacao/TesteTransacao/Transaction.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace TesteTransacao
+{
+    class Transaction
+    {
+        //String de conexão com o banco
+        private string StringCon =
+            @"Data Source = xxxxxx,xxxxxx; 
+            Initial Catalog = xxxxxx;
+            User Id = xxxxxx;
+            Password = xxxxxx;";
+        //@"Data Source = SERVER,PORT; 
+        //Initial Catalog = BD;
+        //User Id = USER; 
+        //Password=PASS;";
+
+        private static SqlConnection con;
+        private static SqlTransaction tran;
+
+
+        //private SqlCommand cmd2 = con.CreateCommand();
+
+        public void transacao(string[] cmds)
+        {
+            using (con = new SqlConnection())
+            {
+                SqlCommand cmd = con.CreateCommand();
+                con.ConnectionString = StringCon;
+                con.Open();
+                tran = con.BeginTransaction();
+
+                try
+                {
+                    foreach (string cmdTxt in cmds)
+                    {
+                        cmd.CommandText = cmdTxt;
+                        Console.WriteLine(cmd.CommandText);
+                        cmd.Transaction = tran;
+                        cmd.ExecuteNonQuery();
+                        Console.WriteLine("Comando executado com sucesso!");
+                    }
+                    tran.Commit();
+                }
+                catch (SqlException ex)
+                {
+                    tran.Rollback();
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+        }
+
+
+    }
+}
